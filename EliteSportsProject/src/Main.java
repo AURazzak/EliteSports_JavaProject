2f30eb8bb2c4b9d4ee0918ef7709007dcfb39ec7
Ahmed Ur Razzak <120051080+AURazzak@users.noreply.github.com>
2024-01-25 01:18:02 +0600

    Add files via upload

---
+++ EliteSportsProject/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+    {
+
+        new RegisterFrom();
+//        new SignIn();
+//        new Menu();
+//        new Cricket();
+//        new Football();
+//        new Basketball();
+//        new FP1();
+//        new FP2();
+//        new Profile();
+    }
+}
